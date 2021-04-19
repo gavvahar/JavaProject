5f2295c078c6b18299cc4b2bd82ae53232eebca2
Natomu <82521403+Natomu@users.noreply.github.com>
2021-04-19 10:17:18 -0400

    Test 23
    Test 23

--- Chess/src/main/Chess.java
+++ Chess/src/main/Chess.java
@@ -50,8 +50,13 @@ public class Chess {
 					
 					
 					
-			
-						Character Piece = '\u265F';
+					
+						//JLabel pawnLabel = new JLabel("\u265F");
+						//jp.add(pawnLabel);
+								
+								
+								
+						//char Piece = '\u265F';
 					
 					
 				
@@ -98,21 +103,36 @@ public class Square extends JPanel {
 		
 		setBackground(this.color);
 		setBorder(new MatteBorder(1, 1, 1, 1, new Color(200, 200, 224)));
-
+		
+		
 	}
 	@Override 
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g.create();
-		//if( )
+		GraphicsEnvironment.getLocalGraphicsEnvironment();
+		if (row == 1 && col < 8) {
+			
+		g2d.setFont(new Font("LucidaSans", Font.PLAIN, 80));
+		g2d.drawString("\u265F", 5, 70);
+		} else if ( row == 6 && col < 8) {
+			g2d.setFont(new Font("LucidaSans", Font.PLAIN, 80));
+			//g2d.setPaint(color.BLACK);
+			g2d.drawString("\u2659", 5, 70);	
+		} else if ( row == 7 && col < 8) {
+			g2d.setFont(new Font("LucidaSans", Font.PLAIN, 80));
+			g2d.setPaint(color.RED);
+			g2d.drawString("\u2658", 5, 70);
+		}
+		//JLabel pawnLabel = new JLabel("\u265F");
+		//jp.add(pawnLabel);
 		
-		
-		Ellipse2D e2d = new Ellipse2D.Double(0.0, 0.0, getWidth() - 2.0, getHeight() - 2.0);
-		g2d.setPaint(Color.BLUE);
-		g2d.fill(e2d);
-
+		//Ellipse2D e2d = new Ellipse2D.Double(0.0, 0.0, getWidth() - 2.0, getHeight() - 2.0);
+		//g2d.setPaint(Color.BLUE);
+		//g2d.fill(e2d);
+		g2d.dispose();
 	}
- }
+}
 
 
 
