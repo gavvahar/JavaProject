bd013b5c71a1b9fa42276983d3ed0663294ce298
Nihar Gavva <nihargavva@gmail.com>
2021-05-06 22:33:54 -0400

    cleaning up but not done yet

--- Chess/src/main/Square.java
+++ Chess/src/main/Square.java
@@ -7,26 +7,17 @@ import javax.swing.border.MatteBorder;
 public class Square extends JPanel
 {
 	private static final long serialVersionUID = 1L;
-	private int row;
-	private int col;
+	private int row, col, place = 7;
 	private Color cellColor;
 	private static Color prevColor;
 	private Piece piece ;
 	private static Square prevSquare ;
 	private static Piece startPiece, endPiece;
-	private static boolean redCastling = true; 
-	private static boolean greenCastling = true;
-	int place = 7;
-	//29
-
+	private static boolean redCastling = true, greenCastling = true;
 	private Square endSpot;
 	private Square startSpot;
-	//
-	private static int startRow,  startCol;
-	private static int endRow,  endCol;
+	private static int startRow, startCol, endRow, endCol;
 	private Board board = Board.getInstance();
-	
-	//
 	private static String highlightedPiece = "";
 
 	public Square(int row, int col, Piece piece)
@@ -36,16 +27,15 @@ public class Square extends JPanel
 		this.row = row;
 		this.col = col;
 		this.piece = piece;
-					
-		//if((row!=0 && col!=0 && (row + col) % 2 == 0 )) {
+
 		if((row + col) % 2 == 0 )
 		{
 			this.cellColor = Color.WHITE;
-		} else
+		}
+		else
 		{
 			this.cellColor = Color.BLACK;
-		}
-			
+		}	
 		setBackground(this.cellColor);
 		setBorder(new MatteBorder(1, 1, 1, 1, new Color(200, 200, 224)));
 	}
@@ -58,12 +48,10 @@ public class Square extends JPanel
 		GraphicsEnvironment.getLocalGraphicsEnvironment();
 
 			g2d.setFont(new Font("LucidaSans", Font.PLAIN, 80));
-			//Piece piece = pieces[row][col];
 			g2d.setPaint(piece.color);
 			g2d.drawString(piece.pieceType, 5, 70);	
 			g2d.dispose();
 	}
-		//26
 	
 	public Color getBackground()
 	{
@@ -79,13 +67,11 @@ public class Square extends JPanel
 	{
 		this.cellColor = color;
 	}
-	//26
 	
 	public Color getCellColor()
 	{
 		return cellColor;
 	}
-	//30
 	
 	public int getRow()
 	{
@@ -107,7 +93,6 @@ public class Square extends JPanel
 		this.col = col;
 	}
 	
-	//
 	public int getStartRow()
 	{
 		return startRow;
@@ -148,78 +133,75 @@ public class Square extends JPanel
 		this.endCol = col;
 	}
 	
-	//public static void setHighlightedPiece(String pieceType) {
-		//highlightedPiece = pieceType;
-	//}
-	
-	//public void setHighlightedPiece(String pieceType) {
-	//	highlightedPiece = new String(pieceType);
-	//}
-    //30
-	
-		
 	public void highlight()
 	{
-		Color color = this.cellColor;	
-		//28
-		//Square startSpot = new Square(row,col,piece);
-			
-		//28
-		//setStartSpot(startSpot);
-		//System.out.println(row + " this is a row");
-		//System.out.println(col + " this is a col");
+		Color color = this.cellColor;
+
 		if (this.piece.pieceType.equals("\u265B"))
 		{
 			highlightedPiece = "\u265B";
-		} else if (this.piece.pieceType.equals("\u265A"))
+		}
+		else
 		{
-			highlightedPiece = "\u265A";
-		} else if (this.piece.pieceType.equals("\u265C"))
-		{
-			highlightedPiece = "\u265C";
-		} else if (this.piece.pieceType.equals("\u265E"))
-		{
-			highlightedPiece = "\u265E";
-		} else if (this.piece.pieceType.equals("\u265D"))
-		{
-			highlightedPiece = "\u265D";
-		} else if (this.piece.pieceType.equals("\u265F"))
-		{
-			highlightedPiece = "\u265F";
-		}	
-	
+			if (this.piece.pieceType.equals("\u265A"))
+			{
+				highlightedPiece = "\u265A";
+			}
+			else
+			{
+				if (this.piece.pieceType.equals("\u265C"))
+				{
+					highlightedPiece = "\u265C";
+				}
+				else
+				{
+					if (this.piece.pieceType.equals("\u265E"))
+					{
+						highlightedPiece = "\u265E";
+					}
+					else
+					{
+						if (this.piece.pieceType.equals("\u265D"))
+						{
+							highlightedPiece = "\u265D";
+						}
+						else
+						{
+							if (this.piece.pieceType.equals("\u265F"))
+							{
+								highlightedPiece = "\u265F";
+							}
+						}
+					}
+				}
+			}
+		}
+		
 		this.startRow = this.row;
 		this.startCol = this.col;
 		this.startPiece = this.piece;
 		this.prevColor = this.cellColor;
 		this.prevSquare = this;
-		//System.out.println("Start Piece : " + startPiece.pieceType );
 		this.cellColor = Color.YELLOW;
-			//highlightedPiece = new String(this.piece.pieceType);
-			
-			//System.out.println(highlightedPiece + " a");
-			
-			//System.out.println(startRow + " this is a StartRow");
-			//System.out.println(startCol + " this is a StartCol");
-			
 			
 		if (color == Color.BLACK || color == Color.WHITE)
 		{
 			setCellColor(Color.YELLOW);
-		} else if ((cellColor == Color.YELLOW) && (row + col) % 2 == 0 )
-		{
-			setCellColor(Color.WHITE);
-		} else if (cellColor == Color.YELLOW)
-		{
-			setCellColor(Color.BLACK);
 		}
-			//28
-	    /*if (piece.pieceType.equals("\u265B"))
+		else
 		{
-	    		System.out.println("It is a queen");
-	    }*/
-	    	//isMoveValid();
-	    	//28
+			if ((cellColor == Color.YELLOW) && (row + col) % 2 == 0 )
+			{
+				setCellColor(Color.WHITE);
+			}
+			else
+			{
+				if (cellColor == Color.YELLOW)
+				{
+					setCellColor(Color.BLACK);
+				}
+			}
+		}
 	}
 	public void highlightEndPoint()
 	{
@@ -231,47 +213,33 @@ public class Square extends JPanel
 			
 		if (color == Color.BLACK || color == Color.WHITE)
 		{
-//			setCellColor(Color.GREEN);
 			
 		}
 		isMoveValid();
-		//JOptionPane.showMessageDialog(null, "THE MOVE IS VALID", "MESSAGE", JOptionPane.INFORMATION_MESSAGE);
 	}
-		//26
 		
-		public void setEndSpot(Square endSpot)
-		{
-			this.endSpot = endSpot;
-		}
+	public void setEndSpot(Square endSpot)
+	{
+		this.endSpot = endSpot;
+	}
 		
-		public Square getEndSpot(int row, int col)
-		{
-			return endSpot;
-		}
+	public Square getEndSpot(int row, int col)
+	{
+		return endSpot;
+	}
 		
-		public void setStartSpot(Square startSpot)
-		{
-			this.startSpot = startSpot;
-		}
+	public void setStartSpot(Square startSpot)
+	{
+		this.startSpot = startSpot;
+	}
 		
-		public Square getStartSpot(int row, int col)
-		{
-			return startSpot;
-		}
-		
-		
-        //28
-        public void isMoveValid()
-		{
-        	//System.out.println(startCol);
-        	//System.out.println(startRow);
-           	//System.out.println(endRow);
-           	//System.out.println(endCol);
-      
-        	
-        	//if (piece.pieceType.equals("\u265F") && pawnMove()) 
-        	//System.out.println(highlightedPiece + " c");
-        	
+	public Square getStartSpot(int row, int col)
+	{
+		return startSpot;
+	}
+
+    public void isMoveValid()
+	{
         	if(highlightedPiece.equals("\u265F") && (pawnMove() == true))
         	{
         		System.out.println("Pawn. Move is valid");
@@ -322,28 +290,28 @@ public class Square extends JPanel
         		System.out.println("King. Move is not valid");
         		JOptionPane.showMessageDialog(null, "The move is not valid", "Output", JOptionPane.ERROR_MESSAGE);
         	}
-        	//	return false;
-        	//}
         }
-        //28
 
 	public boolean pawnMove()
 	{
-		/*System.out.println("Start : " + startRow + ", " + startCol + " End : " + endRow + ", " + endCol);
-		System.out.println("Piece : " + startPiece.pieceType + ", " + endPiece.pieceType);
-		System.out.println("End Piece : "  + endPiece.pieceType);*/
-		if(startPiece.color == Color.GREEN) {
-		 	if (startCol == endCol) {
-				if (((endRow - 1) == startRow) || ( (startRow == 1) && ((endRow - 2) == startRow))){
+		if(startPiece.color == Color.GREEN)
+		{
+		 	if (startCol == endCol)
+			 {
+				if (((endRow - 1) == startRow) || ( (startRow == 1) && ((endRow - 2) == startRow)))
+				{
 	    			if(isColBlocked(startRow, endRow, endCol))
 	    			{
 	    				return false;
-	    			} if (endRow == 7) {
+	    			}
+					
+					if (endRow == 7)
+					{
 	    				startPiece.pieceType = "\u265B";
 	    			}
-	    				movePiece();
-	    				return true;
-					}
+	    			movePiece();
+	    			return true;
+				}
 			} else if ((startCol == (endCol - 1)) || (startCol == (endCol + 1))){
 				if (((endRow - 1) == startRow) && endPiece.color == Color.RED) {
 					killPiece();
